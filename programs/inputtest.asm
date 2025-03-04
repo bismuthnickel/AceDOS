af74474b7747c4de47f74fee6f73aba12a4090dc
bismuth <70106649+bismuthnickel@users.noreply.github.com>
2025-03-04 14:19:36 -0500

    Create inputtest.asm

---
+++ programs/inputtest.asm
@@ -0,0 +1,17 @@
+bits 16
+
+call get_ip
+get_ip:
+    pop bp
+    sub bp, get_ip
+
+main:
+    mov ax, 3
+    int 0x80
+    mov bl, al
+    mov bh, 0x07
+    mov ax, 20
+    int 0x80
+    ret
+
+times (512*8)-($-$$) db 0
